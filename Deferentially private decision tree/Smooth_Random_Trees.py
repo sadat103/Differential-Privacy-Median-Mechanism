20fc6c8f369aa33d76d04f19a139d5a9699c76a3
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2020-08-20 16:48:50 +0600

    k

--- Deferentially private decision tree/Smooth_Random_Trees.py
+++ Deferentially private decision tree/Smooth_Random_Trees.py
@@ -293,20 +293,20 @@ if __name__ == '__main__':
     
         budget_list = []
         accuracy_list = []
-        for i in np.arange(3, 15, 0.9):
+        for i in np.arange(3, 20, 1):
             #forest1forest1 = DP_Random_Forest(train_data1[0:15], test_data1, [16,], 2, 0.1)
             #forest2 = DP_Random_Forest(train_data2[1:4], test_data2, [0,], 2, 0.1)
             #forest3 = DP_Random_Forest(train_data3[1:35], test_data3, [0,], 6, i)
             #forest4 = DP_Random_Forest(train_data4[1:27], test_data4, [0,], 2, 0.1)
-            #forest5 = DP_Random_Forest(train_data5[1:10], test_data5, [0,], 2, 0.1)
-            forest6 = DP_Random_Forest(train_data6[1:4], test_data6, [0,], 12, i)
+            forest5 = DP_Random_Forest(train_data5[1:10], test_data5, [0,], 12, i)
+            #forest6 = DP_Random_Forest(train_data6[1:4], test_data6, [0,], 12, i)
             #forest7 = DP_Random_Forest(train_data7[1:13], test_data7, [0,], 6, i)
             #forest8 = DP_Random_Forest(train_data8[1:10], test_data8, [0,], 15,i)
             #forest9 = DP_Random_Forest(train_data9[1:4], test_data9, [0,], 8, i)
             #forest10 = DP_Random_Forest(train_data10[1:7],test_data10, [0,], 15, i)
             budget_list.append(i)
-            accuracy_list.append(forest6._accuracy*100)
-            print('accuracy = '+str(forest6._accuracy*100))
+            accuracy_list.append(forest5._accuracy*100)
+            print('accuracy = '+str(forest5._accuracy*100))
         print(budget_list)
         print(accuracy_list)
         #print('accuracy = '+str(forest2._accuracy*100))
